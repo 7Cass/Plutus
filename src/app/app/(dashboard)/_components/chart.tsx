626844f755e601e005cda4fce9fe910f606a7522
João Pedro A <jaaopbr@gmail.com>
2024-05-17 15:19:12 -0300

    feat: add initial dashboard page

---
+++ src/app/app/(dashboard)/_components/chart.tsx
@@ -0,0 +1,95 @@
+'use client'
+
+import { ResponsiveContainer, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line } from "recharts";
+
+const data = [
+  {
+    name: 'Jan',
+    expense: 4000,
+    income: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Fev',
+    expense: 3000,
+    income: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Mar',
+    expense: 2000,
+    income: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Abr',
+    expense: 2780,
+    income: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Mai',
+    expense: 1890,
+    income: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Jun',
+    expense: 2390,
+    income: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Jul',
+    expense: 3490,
+    income: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Ago',
+    expense: 3490,
+    income: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Set',
+    expense: 3490,
+    income: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Out',
+    expense: 3490,
+    income: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Nov',
+    expense: 3490,
+    income: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Dez',
+    expense: 3490,
+    income: 4300,
+    amt: 2100,
+  },
+];
+
+export function Chart() {
+  return (
+    <ResponsiveContainer width="100%" height={450}>
+      <LineChart data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="expense" stroke="#8884d8" />
+        <Line type="monotone" dataKey="income" stroke="#82ca9d" />
+      </LineChart>
+    </ResponsiveContainer>
+  )
+}
--- src/app/app/(dashboard)/page.tsx
+++ src/app/app/(dashboard)/page.tsx
@@ -1,20 +1,119 @@
 import {
   DashboardPage,
   DashboardPageHeader,
-  DashboardPageHeaderNav,
   DashboardPageHeaderTitle,
   DashboardPageMain,
 } from "@/components/dashboard/dashboard-page";
-import { PlusIcon } from "@radix-ui/react-icons";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Chart } from "./_components/chart";
+import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
     <DashboardPage>
-      <DashboardPageHeader className="py-2.5">
+      <DashboardPageHeader>
         <DashboardPageHeaderTitle>Dashboard</DashboardPageHeaderTitle>
       </DashboardPageHeader>
 
-      <DashboardPageMain>Data</DashboardPageMain>
+      <DashboardPageMain className="grid gap-4">
+        <div className="grid grid-cols-4 gap-4">
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle className="text-2xl">UNDER CONSTRUCTION</CardTitle>
+              <CardContent className="p-0">
+                <h1 className="text-xl">NO INFO YET.</h1>
+              </CardContent>
+            </CardHeader>
+          </Card>
+          <Card className="col-span-1">
+            <CardHeader>
+              <div className="flex flex-wrap items-center gap-4 py-2">
+                <h1 className="flex-1 text-4xl font-semibold">Income</h1>
+                <span className="text-2xl font-light">R$: 4250,92</span>
+                <ArrowUpIcon className="w-10 h-10 text-green-500" />
+              </div>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <div className="pt-4 border-t border-border space-y-2">
+                <h2 className="text-lg">Recent transactions</h2>
+                <div className="w-full space-y-2">
+                  <div className="flex justify-between gap-2 border border-border rounded p-2">
+                    <p>R$: 250,09</p>
+                    <p>12/04/2023</p>
+                    <Badge>Store</Badge>
+                  </div>
+                  <div className="flex justify-between gap-2 border border-border rounded p-2">
+                    <p>R$: 250,09</p>
+                    <p>12/04/2023</p>
+                    <Badge>Market</Badge>
+                  </div>
+                  <div className="flex justify-between gap-2 border border-border rounded p-2">
+                    <p>R$: 250,09</p>
+                    <p>12/04/2023</p>
+                    <Badge>Steam</Badge>
+                  </div>
+                  <div className="flex justify-between gap-2 border border-border rounded p-2">
+                    <p>R$: 250,09</p>
+                    <p>12/04/2023</p>
+                    <Badge>Salary</Badge>
+                  </div>
+                </div>
+                <Button variant="secondary" className="w-full">See more</Button>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="col-span-1">
+            <CardHeader>
+              <div className="flex flex-wrap items-center gap-4 py-2">
+                <h1 className="flex-1 text-4xl font-semibold">Expense</h1>
+                <span className="text-2xl font-light">R$: 4250,92</span>
+                <ArrowDownIcon className="w-10 h-10 text-red-500" />
+              </div>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <div className="pt-4 border-t border-border space-y-2">
+                <h2 className="text-lg">Recent transactions</h2>
+                <div className="w-full space-y-2">
+                  <div className="flex justify-between gap-2 border border-border rounded p-2">
+                    <p>R$: 250,09</p>
+                    <p>12/04/2023</p>
+                    <Badge>Store</Badge>
+                  </div>
+                  <div className="flex justify-between gap-2 border border-border rounded p-2">
+                    <p>R$: 250,09</p>
+                    <p>12/04/2023</p>
+                    <Badge>Market</Badge>
+                  </div>
+                  <div className="flex justify-between gap-2 border border-border rounded p-2">
+                    <p>R$: 250,09</p>
+                    <p>12/04/2023</p>
+                    <Badge>Steam</Badge>
+                  </div>
+                  <div className="flex justify-between gap-2 border border-border rounded p-2">
+                    <p>R$: 250,09</p>
+                    <p>12/04/2023</p>
+                    <Badge>Salary</Badge>
+                  </div>
+                </div>
+                <Button variant="secondary" className="w-full">See more</Button>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Chart</CardTitle>
+              <CardDescription>Income/Expense</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Chart />
+            </CardContent>
+          </Card>
+        </div>
+      </DashboardPageMain>
     </DashboardPage>
   );
 }
